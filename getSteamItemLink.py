a3112be427a6944bb268222798a0eefc2fec328b
guiandc <g.andradc@gmail.com>
2023-11-19 11:50:05 -0300

    removing old codes

--- getSteamItemLink.py
+++
@@ -1,7 +0,0 @@
-def getSteamItemLink(item):
-    finalLink = 'https://steamcommunity.com/market/listings/730/' + item \
-                .replace(' ', '%20')\
-                .replace('|', '%7C')\
-                .replace('(', '%28')\
-                .replace(')', '%29')
-    return str(finalLink)
